64566449650f0542066ec11eadbf496a719121a2
Paola Bechalani <paobechluci@gmail.com>
2025-07-04 15:13:36 -0400

    sign up page is working

--- chompquest-frontend/src/App.tsx
+++ chompquest-frontend/src/App.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// Import your SignUp component.
-// Make sure the path is correct based on where you saved SignUp.tsx (e.g., './components/SignUp' or './SignUp')
-import SignUp from './components/SignUp'; // Assuming it's in src/components
+import SignUp from './components/SignUp'; 
+import './App.css'; 
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          {/* This route will display your SignUp component when the URL is /signup */}
-          <Route path="/signup" element={<SignUp />} />
-
-          {/* This route will display your SignUp component when the URL is the root (/) */}
-          {/* This means when you go to http://localhost:5173/, you'll see the signup page */}
-          <Route path="/" element={<SignUp />} />
-
-          {/* You can remove or comment out any other routes for now, like a placeholder login page */}
-          {/* When you're ready to create the Login page, you'll add its route here. */}
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      {/*will need to introduce routing when navigating between pages*/}
+      <SignUp />
+    </div>
   );
 }
 
